4f6d97b81b9d7df1bb75b89cd9f961a34efc46c1
Utsoft7 <kumar.utkarsh.1574@outlook.com>
2024-06-02 17:40:05 +0530

    deployed

---
+++ scripts/data.js
@@ -0,0 +1,95 @@
+const linkedin = "https://www.linkedin.com/in/utkarsh-kumar-a42028251/";
+const github = "https://github.com/Utsoft7";
+const email = "mailto:phoenix.fireup7845@gmail.com";
+const twitter = "https://x.com/Resilient_Ut?t=SS54xnX__PPyOAh3JfiHYQ&s=35";
+var today = new Date();
+
+about = [
+  "<br>",
+  "Hey there, I'm Utkarsh Kumar. 👋",
+  "<br>",
+  "I'm a Engineering Student at KIIT University, Bhubaneswar,.",
+  "I love to build things that are useful and can have a positive impact on other people's lives.",
+  "Having said that, I'm always ready to work on quirky projects",
+  "I am always ready to explore new things and collaborate with others.",
+  "I am highly interested in the world of Finance and Business..",
+  "Gathering a lot of knowledge and insights about the World.",
+  "Outside of the professional space, I love playing TableTennis and casual PC Gaming.",
+  "<br>",
+  "Hope you explore and enjoy this Website! 🙏🏻",
+  "<br>",
+];
+
+banner = [
+  "<br>",
+  '<span class = "index">' + String(today) + "</span>",
+  '<span class="index">@Utkarsh Kumar©️ </span>',
+  "<br>",
+
+  " __    __    __      __                                      __",
+  "/  |  /  |  /  |    /  |                                    /  |",
+  "$$ |  $$ | _$$ |_   $$ |   __   ______    ______    _______ $$ |____",
+  "$$ |  $$ |/ $$   |  $$ |  /  | /      \\  /      \\  /       |$$      \\",
+  "$$ |  $$ |$$$$$$/   $$ |_/$$/  $$$$$$  |/$$$$$$  |/$$$$$$$/ $$$$$$$  |",
+  "$$ |  $$ |  $$ | __ $$   $$<   /    $$ |$$ |  $$/ $$      \\ $$ |  $$ |",
+  "$$ \\__$$ |  $$ |/  |$$$$$$  \\ /$$$$$$$ |$$ |       $$$$$$  |$$ |  $$ |",
+  "$$    $$/   $$  $$/ $$ | $$  |$$    $$ |$$ |      /     $$/ $$ |  $$ |",
+  " $$$$$$/     $$$$/  $$/   $$/  $$$$$$$/ $$/       $$$$$$$/  $$/   $$/",
+
+  "<br>",
+  '<span class="color2">Welcome to my Terminal Website</span>',
+  '<span class="color2">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+  '<span class="color2">To know more about the contents,</span> <span class="command">type \'cat\' followed by what you want to know </span><span class="color2">.</span>',
+
+  // '<span class="color2"></span>',
+  "<br>",
+];
+
+help = [
+  "<br>",
+  '👦🏻  <span class="command">About</span>          About Me',
+  '🌏  <span class="command">Social</span>         Display social networks',
+  '🏗  <span class="command">Projects</span>        View coding projects',
+  '📧  <span class="command">Email</span>          Reach me through email',
+  '🧹  <span class="command">Clear</span>          Clear the terminal',
+  '❓  <span class="command">Help</span>           Open the help menu',
+  '🕰  <span class="command">History</span>         View command history',
+  '👋  <span class="command">Exit</span>           Close the tab and exit',
+  "<br>",
+];
+
+social = [
+  "<br>",
+  "Enter the name of any of the following or simply click the links...",
+  "<br>",
+  'Linkedin       <a href="' +
+    linkedin +
+    '" target="_blank">linkedin/Utkarsh Kumar' +
+    "</a>",
+  'Github         <a href="' +
+    github +
+    '" target="_blank">github/Utsoft7' +
+    "</a>",
+  'Twitter        <a href="' +
+    twitter +
+    '" target="_blank">twitter/Resilient_Ut' +
+    "</a>",
+  "<br>",
+];
+projects = [
+  "<br>",
+  '<span class="command">Still Working on them, WORK IN PROGRESS </span>',
+  /*'<span class="command">1. WeatherPedia</span>',
+  "<br>",
+  "<br>",
+  '<span class="command">2. Mental Health Fitness Tracker',
+  "<br>",
+  "<br>",
+  '<span class="command">3. Portfolio Website',
+  "<br>",
+  "<br>",
+  //'<span class="command">4. Old Portfolio Website (This Website)</span>',
+  "<br>",*/
+  'Click the following links to access all my projects: <a href = "https://github.com/Utsoft7" target="_blank">(Github)</a>',
+  "<br>",
+];
